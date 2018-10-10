ab63cadf78c30b3b3d504ec1b323175666bbdeec
Dejan Savuljesku <d.savuljesku@gmail.com>
2018-10-10 08:58:34 +0200

    Update "add portlet" button
    Change-Id: I90604381610428d100adddb13b12c4c052856f54
    ERM: #10468

--- resources/bluespice.dashboards.portletcatalog.js
+++ resources/bluespice.dashboards.portletcatalog.js
@@ -1,12 +1,14 @@
 if ( mw.config.get('bsPortalConfigLocation') !== null ) {
-	var image = '<img src="' + mw.config.get( "wgScriptPath" ) + '/extensions/BlueSpiceFoundation/resources/bluespice/images/bs-m_add.png" />';
-	var text = $( 'h1.firstHeading' ).html();
-	var anchor = $( 'h1.firstHeading' ).html(
-		text + '<a href="#" id="bs-dashboard-add">' + image + '</a>'
-	);
-	$(anchor).children('#bs-dashboard-add').on( 'click', function() {
-		Ext.require( 'BS.Dashboards.PortletCatalog', function() {
-			BS.Dashboards.PortletCatalog.show();
+	var imageUrl = mw.config.get( "wgScriptPath" ) + '/extensions/BlueSpiceFoundation/resources/bluespice/images/bs-btn_add.png';
+
+	$origHeading = $( 'h1.firstHeading' );
+	$addButton = $( '<span>' )
+		.attr( 'id', 'bs-dashboard-add' )
+		.attr( 'style', 'background-image: url(' + imageUrl + ')' )
+		.on( 'click', function() {
+			Ext.require( 'BS.Dashboards.PortletCatalog', function() {
+				BS.Dashboards.PortletCatalog.show();
+			} );
 		} );
-	} );
+	$origHeading.append( $addButton );
 }
