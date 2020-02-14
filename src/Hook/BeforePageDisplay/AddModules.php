1483fa2df19be5598bb3ca5c3e8026cfd5a0c940
pwirth <wirth@hallowelt.biz>
2020-02-14 12:22:08 +0100

    Add resources conditionally
    ERM:18233
    [3.1.x]
    
    Change-Id: Iad794442329ec2795c6879bc584d82242bf97f6c

--- src/Hook/BeforePageDisplay/AddModules.php
+++ src/Hook/BeforePageDisplay/AddModules.php
@@ -6,9 +6,23 @@ use BlueSpice\Hook\BeforePageDisplay;
 
 class AddModules extends BeforePageDisplay {
 
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function skipProcessing() {
+		return !$this->out->getUser()->getOption(
+			'bs-dashboards-pref-userdashboardonlogo',
+			false
+		);
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
 	protected function doProcess() {
 		$this->out->addModules( 'ext.bluespice.dashboards' );
-		$this->out->addModuleStyles( 'ext.bluespice.dashboards.styles' );
 		return true;
 	}
 
