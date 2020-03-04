79007c6b152cad41f0da3c4db36fb138d2a17700
DannyS712 <DannyS712.enwiki@gmail.com>
2020-03-04 22:35:16 +0000

    Replace uses of old deprecated SpecialPageFactory
    Bug: T246142
    Change-Id: I0a9f24cbee5b83cf589fd71c59475f3dfbe97f19

--- src/Hook/PersonalUrls/AddDashboardUrls.php
+++ src/Hook/PersonalUrls/AddDashboardUrls.php
@@ -13,16 +13,17 @@ class AddDashboardUrls extends PersonalUrls {
 
 	protected function doProcess() {
 		$user = $this->getContext()->getUser();
+		$spFactory = \MediaWiki\MediaWikiServices::getInstance()->getSpecialPageFactory();
+
 		$this->personal_urls['userdashboard'] = [
 			'href' => \SpecialPage::getTitleFor( 'UserDashboard' )->getLocalURL(),
-			'text' => \SpecialPageFactory::getPage( 'UserDashboard' )->getDescription()
+			'text' => $spFactory->getPage( 'UserDashboard' )->getDescription()
 		];
 
 		if ( in_array( 'sysop', $user->getGroups() ) ) {
-
 			$this->personal_urls['admindashboard'] = [
 				'href' => \SpecialPage::getTitleFor( 'AdminDashboard' )->getLocalURL(),
-				'text' => \SpecialPageFactory::getPage( 'AdminDashboard' )->getDescription()
+				'text' => $spFactory->getPage( 'AdminDashboard' )->getDescription()
 			];
 		}
 		return true;
