3b249eb3e6c421e1c102d2cc3dcfb0cd523d0453
pwirth <wirth@hallowelt.biz>
2021-04-15 15:51:54 +0200

    Fix resource run-time error and object when array is expected
    ERM:23548
    [4.x]
    
    Change-Id: Ifec53b87cf7d86d7fa6ec0b003b0cc6f855fc7bf

--- resources/bluespice.dashboards.userDashboard.js
+++ resources/bluespice.dashboards.userDashboard.js
@@ -1,9 +1,10 @@
-mw.loader.using( mw.config.get( 'bsPortalDependencies' ) ).then( function() {
-	Ext.onReady( function(){
-		Ext.create('BS.Dashboards.DashboardPanel', {
-			renderTo: 'bs-dashboards-userdashboard',
-			portalConfig: mw.config.get( 'bsPortalConfig' )
+( function ( mw ) {
+	mw.loader.using( mw.config.get( 'bsPortalDependencies' ), [] ).done( function() {
+		Ext.onReady( function () {
+			Ext.create( 'BS.Dashboards.DashboardPanel', {
+				renderTo: 'bs-dashboards-userdashboard',
+				portalConfig: mw.config.get( 'bsPortalConfig' )
+			} );
 		} );
 	} );
-} );
-
+} )( mediaWiki );
