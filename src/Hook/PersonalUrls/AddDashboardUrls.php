c9a7878610157a3538e43bb40b5675373482e263
Alexander Vorwerk <alec@vc-celle.de>
2021-05-31 17:30:40 +0200

    Avoid using User::getGroups()
    User::getGroups() is deprecated and should be replaced with
    UserGroupManager::getGroups()
    
    Bug: T281851
    Change-Id: If1aab2a7273b4862f124f3952a8f489eac0a3f65

--- src/Hook/PersonalUrls/AddDashboardUrls.php
+++ src/Hook/PersonalUrls/AddDashboardUrls.php
@@ -3,6 +3,7 @@
 namespace BlueSpice\Dashboards\Hook\PersonalUrls;
 
 use BlueSpice\Hook\PersonalUrls;
+use MediaWiki\MediaWikiServices;
 
 class AddDashboardUrls extends PersonalUrls {
 
@@ -13,14 +14,16 @@ class AddDashboardUrls extends PersonalUrls {
 
 	protected function doProcess() {
 		$user = $this->getContext()->getUser();
-		$spFactory = \MediaWiki\MediaWikiServices::getInstance()->getSpecialPageFactory();
+		$services = MediaWikiServices::getInstance();
+		$spFactory = $services->getSpecialPageFactory();
+		$userGroupManager = $services->getUserGroupManager();
 
 		$this->personal_urls['userdashboard'] = [
 			'href' => \SpecialPage::getTitleFor( 'UserDashboard' )->getLocalURL(),
 			'text' => $spFactory->getPage( 'UserDashboard' )->getDescription()
 		];
 
-		if ( in_array( 'sysop', $user->getGroups() ) ) {
+		if ( in_array( 'sysop', $userGroupManager->getUserGroups( $user ) ) ) {
 			$this->personal_urls['admindashboard'] = [
 				'href' => \SpecialPage::getTitleFor( 'AdminDashboard' )->getLocalURL(),
 				'text' => $spFactory->getPage( 'AdminDashboard' )->getDescription()
